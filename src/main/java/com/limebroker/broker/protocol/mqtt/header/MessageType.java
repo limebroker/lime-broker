40d94fe4db19cdc7ac0a4b84bf0e570babb36e1d
Martyn Taylor <mtaylor@redhat.com>
2014-05-09 15:56:45 +0100

    Added methods to get MessageType enum from byte code

--- src/main/java/com/limebroker/broker/protocol/mqtt/header/MessageType.java
+++ src/main/java/com/limebroker/broker/protocol/mqtt/header/MessageType.java
@@ -1,18 +1,47 @@
 package com.limebroker.broker.protocol.mqtt.header;
 
-public enum MessageType
-{
-    CONNECT,
-    CONNACK,
-    PUBLISH,
-    PUBACK,
-    PUBREC,
-    PUBREL,
-    PUBCOMP,
-    SUBSCRIBE,
-    UNSUBSCRIBE,
-    UNSUBACK,
-    PINGREQ,
-    PINGRESP,
-    DISCONNECT
+public enum MessageType {
+    CONNECT((byte) 1), CONNACK((byte) 2), PUBLISH((byte) 3), PUBACK((byte) 4), PUBREC(
+            (byte) 5), PUBREL((byte) 6), PUBCOMP((byte) 7), SUBSCRIBE((byte) 8), SUBACK(
+            (byte) 9), UNSUBSCRIBE((byte) 10), UNSUBACK((byte) 11), PINGREQ(
+            (byte) 12), PINGRESP((byte) 13), DISCONNECT((byte) 14);
+
+    private final byte code;
+
+    private MessageType(byte code) {
+        this.code = code;
+    }
+
+    public byte getCode() {
+        return code;
+    }
+
+    /**
+     * Returns a MessageType based MessageType 4bit code. As specified in MQTT
+     * Spec.
+     * 
+     * @param code
+     * @return
+     */
+    public static MessageType getMessageTypeFromByte(byte code) {
+        for (MessageType mt : MessageType.values()) {
+            if (mt.code == code) {
+                return mt;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns a MessageType based on the 4bit byte code as it is read from the
+     * wire.
+     * 
+     * @param code
+     * @return
+     */
+    public static MessageType getMessageTypeFromWireByte(byte code) {
+        code >>>= 4;
+        code &= 0b00001111;
+        return getMessageTypeFromByte(code);
+    }
 }
---
+++ src/test/java/com/limebroker/broker/protocol/mqtt/header/MessageTypeTest.java
@@ -0,0 +1,24 @@
+package com.limebroker.broker.protocol.mqtt.header;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MessageTypeTest {
+
+    @Test
+    public void getMessageTypeFromByteTest() {
+        assertEquals(MessageType.CONNECT,
+                MessageType.getMessageTypeFromByte((byte) 0b00000001));
+        assertEquals(MessageType.DISCONNECT,
+                MessageType.getMessageTypeFromByte((byte) 0b00001110));
+    }
+
+    @Test
+    public void getMessageTypeFromWireByteTest() {
+        assertEquals(MessageType.CONNECT,
+                MessageType.getMessageTypeFromWireByte((byte) 0b00011111));
+        assertEquals(MessageType.DISCONNECT,
+                MessageType.getMessageTypeFromWireByte((byte) 0b11100000));
+    }
+}
